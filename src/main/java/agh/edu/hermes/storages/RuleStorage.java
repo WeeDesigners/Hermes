5e20e9e77eb3485ca5b85669c1f2d40efc8071dc
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-10-04 22:40:54 +0200

    Created RuleStorage.java class

---
+++ src/main/java/agh/edu/hermes/storages/RuleStorage.java
@@ -0,0 +1,43 @@
+package agh.edu.hermes.storages;
+
+import agh.edu.hermes.types.Rule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuleStorage {
+
+    private static final RuleStorage instance = new RuleStorage();
+
+    private final List<Rule> rules;
+
+    public RuleStorage() {
+        rules = new ArrayList<>();
+    }
+
+    public static RuleStorage getInstance() {
+        return instance;
+    }
+
+    public boolean addRule(Rule rule) {
+        if(getRule(rule.id) == null){
+            rules.add(rule);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Rule> getRules() {
+        return new ArrayList<>(rules);
+    }
+
+    public Rule getRule(long id){
+        for(Rule rule : rules){
+            if(rule.id == id){
+                return rule;
+            }
+        }
+        return null;
+    }
+
+}
