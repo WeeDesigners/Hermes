49696d879b96fb06e1a4a043c954a3fd3cc98ad2
socz3qqq <soczawa@student.agh.edu.pl>
2024-10-06 11:47:29 +0200

    fixed linux docket support

---
+++ scripts/build-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+app_version=$(ls -t target | grep Hermes | head -n1)
+echo "$app_version"
+mvn clean install
+docker build --no-cache --build-arg JAR_FILE_ARG="$app_version" -t weedesigners/    hermes .
