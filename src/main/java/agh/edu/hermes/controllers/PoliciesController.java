4ccc76ddf5a8fcca24e5a0b1cae640718399719e
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-10-06 20:34:31 +0200

    Implemented PoliciesController.java class

--- src/main/java/agh/edu/hermes/controllers/PoliciesController.java
+++ src/main/java/agh/edu/hermes/controllers/PoliciesController.java
@@ -1,15 +1,37 @@
 package agh.edu.hermes.controllers;
 
 
+import agh.edu.hermes.services.PoliciesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/policies")
 public class PoliciesController {
 
+    private final PoliciesService policiesService;
 
+    @Autowired
+    public PoliciesController(PoliciesService policiesService) {
+        this.policiesService = policiesService;
+    }
+
+    @PostMapping("/add/{id}")
+    public String addRuleToPolicies(@PathVariable("id") long id){
+        if(policiesService.addRuleToPoliciesById(id)){
+            return "Rule id=" + id + " added successfully to policies";
+        }
+        return "Rule id=" + id + " not added to policies";
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public String removeRuleFromPolicies(@PathVariable("id") long id){
+        if(policiesService.removeRuleFromPolicies(id)){
+            return "Rule id=" + id + " is removed from policies";
+        }
+        return "Rule id=" + id + " is not removed from policies";
+    }
 
 
 
