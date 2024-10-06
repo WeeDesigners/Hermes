c9cc9d3da1e71e5531e9c31b3d072be197c70f94
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-10-06 20:27:34 +0200

    Implemented SlaController.java class with 2 endpoints

--- src/main/java/agh/edu/hermes/controllers/SlaController.java
+++ src/main/java/agh/edu/hermes/controllers/SlaController.java
@@ -1,13 +1,35 @@
 package agh.edu.hermes.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import agh.edu.hermes.services.SlaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sla")
 public class SlaController {
 
+    private final SlaService slaService;
 
+    @Autowired
+    public SlaController(SlaService slaService) {
+        this.slaService = slaService;
+    }
+
+    @PostMapping("/add/{id}")
+    public String addRuleToSla(@PathVariable("id") long id){
+        if(slaService.addRuleToSlaById(id)){
+            return "Rule id=" + id + " added successfully to SLA";
+        }
+        return "Rule id=" + id + " not added to SLA";
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public String removeRuleFromSla(@PathVariable("id") long id){
+        if(slaService.removeRuleFromSla(id)){
+            return "Rule id=" + id + " is removed from SLA";
+        }
+        return "Rule id=" + id + " is not removed from SLA";
+    }
 
 
 }
