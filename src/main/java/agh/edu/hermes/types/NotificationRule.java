5bde07cb55929cd40cc318497aae0d675d05c849
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-10-06 20:51:40 +0200

    Added empty constructors to Rule and NotificationRule classes (due to gson implementation issue)

--- src/main/java/agh/edu/hermes/types/NotificationRule.java
+++ src/main/java/agh/edu/hermes/types/NotificationRule.java
@@ -7,6 +7,11 @@ public class NotificationRule extends Rule {
 
     public final String email;
 
+    public NotificationRule() {
+        super();
+        this.email = "";
+    }
+
     public NotificationRule(RuleAttribute attribute, RuleSubject subject, List<Number> value, UnitType unit, ActionType action, String email) {
         super(attribute, subject, value, unit, action);
         this.email = email;
