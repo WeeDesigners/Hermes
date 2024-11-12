495a786bef1bf3b625292b87a64ff4d688c8ff89
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-11-12 23:04:52 +0100

    Changed conditions and added new ActionType

--- src/main/java/agh/edu/hermes/types/attributes/Action.java
+++ src/main/java/agh/edu/hermes/types/attributes/Action.java
@@ -2,28 +2,41 @@ package agh.edu.hermes.types.attributes;
 
 
 public enum Action {
-
     DEFAULT {
         @Override
         public void request(Params params) {
             System.out.println("Default action - requested");
         }
+
     },
     ACTION_1 {
         @Override
         public void request(Params params) {
-            //TODO
+            // TODO
             System.out.println("Action 1 - requested");
         }
+
     },
     ACTION_2 {
         @Override
         public void request(Params params) {
-            //TODO
+            // TODO
             System.out.println("Action 2 - requested");
         }
+
+    },
+
+    KubernetesChangeResourcesOfContainerWithinDeploymentAction {
+        @Override
+        public void request(Params params) {
+            // TODO
+            System.out.println("Action 2 - requested");
+        }
+
+
     },
     ;
 
     public abstract void request(Params params);
+
 }
