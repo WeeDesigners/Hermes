b93a47721cf9fef53a6a0c3d955d3e79100e9dda
socz3qqq <soczawa@student.agh.edu.pl>
2024-12-17 23:03:02 +0100

    fix hermes deployment

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,4 +2,4 @@
 app_version=$(ls -t target | grep Hermes | head -n1)
 echo "$app_version"
 mvn clean install
-docker build --no-cache --build-arg JAR_FILE_ARG="$app_version" -t weedesigners/    hermes .
+docker build --no-cache --build-arg JAR_FILE_ARG="$app_version" -t weedesigners/hermes:latest .
